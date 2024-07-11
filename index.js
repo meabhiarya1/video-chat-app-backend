db16226f7fea7d4119b0a795e2fc08ae955bf09e
Abhishek kumar <abhi.arya97161@gmail.com>
2024-07-11 18:04:42 +0530

    socket io and camera access given

--- index.js
+++ index.js
@@ -17,10 +17,12 @@ app.use(cors());
 app.use(express.json());
 
 const emailToSocketMapping = new Map();
+const socketToEmailMapping = new Map();
 
 io.on("connection", (socket) => {
   console.log("New Connection");
-
+  
+  //   for listening we use "on" and for sending we use "emit"
   socket.on("join-room", (data) => {
     const { roomID, emailID } = data;
     if (!roomID || !emailID) {
@@ -30,8 +32,23 @@ io.on("connection", (socket) => {
 
     console.log(`User ${emailID} joined Room ${roomID}`);
     emailToSocketMapping.set(emailID, socket.id);
+    socketToEmailMapping.set(socket.id, emailID);
     socket.join(roomID);
+    socket.emit("joined-room", { roomID });
     socket.broadcast.to(roomID).emit("user-joined", { emailID });
+  });
+
+  socket.on("call-user", (data) => {
+    const { emailID, offer } = data;
+    const fromEmail = socketToEmailMapping.get(socket.id);
+    const socketID = emailToSocketMapping.get(emailID);
+    socket.to(socketID).emit("incoming-call", { from: fromEmail, offer });
+  });
+
+  socket.on("call-accepted", (data) => {
+    const { emailID, ans } = data;
+    const socketId = emailToSocketMapping.get(emailID);
+    socket.to(socketId).emit("call-accepted", { ans });
   });
 });
 
